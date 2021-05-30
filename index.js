cfc8721c848cae446fc62b381cd4086e713fc24e
DarshSikka <67545017+DarshSikka@users.noreply.github.com>
2021-05-30 19:43:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const joi=require("joi");
 const express=require('express')
 const app=express();
 const port=process.env.PORT||3000;
+const days=[31,29,31,30,31,30,31,31,30,31,30,31];
 const my_server=(data)=>{
 app.get('/', (req, res)=>{
     let to_send="<h1>Events</h1>";
@@ -24,42 +25,44 @@ app.get('/json', (req, res)=>{
 })
 }
 const listen=()=>{
-    app.listen(port, ()=>{console.log(`Go to port ${port} for viewing response or port${port}/json for viewing json data`)})
+    app.listen(port, ()=>{console.log('\x1b[36m%s\x1b[0m', `Go to port ${port} for viewing response or port${port}/json for viewing json data]`)})
 }
-const schema=joi.object( {
+const schema=joi.object({
    month:joi.number().min(1).max(12).required()
 })
-const http=require('http');
-const days=[31,29,31,30,31,30,31,31,30,31,30,31]
-const readline=require("readline").createInterface({
-    input:process.stdin,
-    output:process.stdout
-})
-    
-readline.question("Enter a month", month=>{
-    const result=schema.validate({month:month});
-    if(result.error){
-    console.log(result.error.details[0].message);
-    process.exit();
+const month=process.argv[2]
+const day=process.argv[3]
+if(!month){
+  console.log("Month is required please re-enter");
+  process.exit()
+}
+if(!day){
+  console.log("Day is required please re-enter")
+  process.exit()
+}
+const result=schema.validate({month:month});
+if(result.error){
+console.log(result.error.details[0].message);
+process.exit();
+}
+    const day_schema=joi.object({
+        day:joi.number().max(days[month-1]).min(1)
+    });
+    const second=day_schema.validate({day:day});
+    if(second.error){
+        console.log(second.error.details[0].message);
+        console.log("Process exited");
+        process.exit()
     }
-    readline.question("Enter a Day", day=>{
-        const day_schema=joi.object({
-            day:joi.number().max(days[month-1]).min(1)
-        });
-        const second=day_schema.validate({day:day});
-        if(second.error){
-            console.log(second.error.details[0].message);
-            console.log("Process exited");
-            process.exit()
-        }
-        const url= `https://history.muffinlabs.com/date/${month}/${day}`
-        fetch(url).then(
-            (response)=>response.text().then((text)=>{
-              const obj=JSON.parse(text);
-              const data=obj.data;
-              my_server(data);
-              listen();
-            }))
-    }    
-        );
-    })
+    const url= `https://history.muffinlabs.com/date/${month}/${day}`
+    console.log("Loading Data……")
+    fetch(url).then(
+        (response)=>response.text().then((text)=>{
+          const obj=JSON.parse(text);
+          const data=obj.data;
+          my_server(data);
+          console.log(`Oldest Event: ${data.Events[0].html}`);
+          console.log(`Oldest Birth: ${data.Births[0].html}`);
+          console.log(`Oldest Death: ${data.Deaths[0].html}`)
+          listen();
+        }));
